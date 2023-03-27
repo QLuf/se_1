3a2f3ae8dc7888d3285bfe2c720543b1748cdeca
ThuQuyen <37749393+QLuf@users.noreply.github.com>
2023-03-27 10:44:22 +0700

    se_1_web

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","ui"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/SE_33.fire",orientation:"landscape",jsList:[]};
